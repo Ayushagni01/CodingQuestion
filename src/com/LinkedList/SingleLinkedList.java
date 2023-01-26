d74769cf8c0c3c0261fd408eb25a476df11f6044
Ayushagni01 <ayush.ayush24@gmail.com>
2023-01-26 10:03:05 +0530

    Single linked List added

--- src/com/LinkedList/SingleLinkedList.java
+++ src/com/LinkedList/SingleLinkedList.java
@@ -13,7 +13,7 @@ public class SingleLinkedList {
 
     }
 
-    Node head=null;
+   public Node head=null;
     //now concentrate on the following things
 
     public static void main(String[] args) {
@@ -30,9 +30,41 @@ public class SingleLinkedList {
         //print the linkedlist using the iterative and non iterative approached
        // printListed(l.head);
         //print the node using recursion
-        printNode(l.head);
+       // printNode(l.head);
+        //print length using loop
+        //printLength(l.head);
+        //print the length using recursion
+        //int len=printLengthRecurison(l.head);
+        //System.out.println("print the length is "+len);
+        //add the node in the begining of the node
+       // addNodeatBegining(l.head,30);
+       //insert node at the end of the linkedlist
+        insertNodeAtEnd(l.head,100);
+
 
     }
+    //insert a node in the linkedList
+    public static void addNodeatBegining(Node head,int data)
+    {
+        Node newNode=new Node(data);
+        newNode.next=head;
+        head=newNode;
+        printNode(head);
+    }
+
+    public static void insertNodeAtEnd(Node head,int data){
+        Node newNode=new Node(data);
+        Node curr=head;
+        Node prev=null;
+        while(curr!=null){
+            prev=curr;
+            curr=curr.next;
+        }
+        System.out.println(prev.data);
+
+
+    }
+
 
     //using loop
     public static void printListed(Node head){
@@ -51,6 +83,23 @@ public class SingleLinkedList {
         }
         System.out.print(head.data+"-->");
         return printNode(head.next);
+    }
+    //print the length of the singly linked list
+    public static void printLength(Node head){
+        int c=0;
+        while(head!=null){
+            c++;
+            head=head.next;
+         }
+        System.out.println("The length of linked list is "+c);
+
+    }
+    //print the length using the recursion
+    public static int printLengthRecurison(Node head){
+        if(head==null)return 0;
+
+        return 1+ printLengthRecurison(head.next);
+
 
     }
 
