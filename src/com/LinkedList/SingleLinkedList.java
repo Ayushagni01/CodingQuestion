87a14ecb09456387aed5fe1414f5087aa40d352c
Ayushagni01 <ayush.ayush24@gmail.com>
2023-01-26 08:23:50 +0530

    linkedlist node added

--- src/com/LinkedList/SingleLinkedList.java
+++ src/com/LinkedList/SingleLinkedList.java
@@ -2,7 +2,7 @@ package com.LinkedList;
 
 public class SingleLinkedList {
 
-    class Node{
+   static class Node{
         int data;
         Node next;
 
@@ -16,6 +16,28 @@ public class SingleLinkedList {
     Node head=null;
     //now concentrate on the following things
 
+    public static void main(String[] args) {
 
+
+        SingleLinkedList l=new SingleLinkedList();
+        l.head=new Node(5);
+        Node second=new Node(10);
+        Node third=new Node(20);
+        l.head.next=second;
+        second.next=third;
+
+
+        //print the linkedlist using the iterative and non iterative approached
+        printListed(l.head);
+
+    }
+
+    //using loop
+    public static void printListed(Node head){
+        while(head!=null){
+            System.out.println(head.data);
+            head=head.next;
+        }
+    }
 
 }
