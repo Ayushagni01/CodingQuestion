ecfe18d453c46b5dde872dd3bb379c3f95c1d706
Ayushagni01 <ayush.ayush24@gmail.com>
2023-01-26 13:55:51 +0530

    Single linked List added

--- src/com/LinkedList/SingleLinkedList.java
+++ src/com/LinkedList/SingleLinkedList.java
@@ -44,8 +44,49 @@ public class SingleLinkedList {
         //insert data at the end
        // insertAtEndNode1(l.head, 120);
         //insert a node at the given positions\
-        insertAtPosition(l.head,40,3);
+        //insertAtPosition(l.head,40,3);
+        //delete a node in the linked list
+        //deleteFirstNode(l.head);
+        //delete at the end of the node
+        deleteAtTheEndOfLinkedList(l.head);
     }
+
+    //delete the first node in the single list
+    public static void deleteFirstNode(Node head){
+
+        if(head==null) return;
+        Node temp=head;
+        head=head.next;
+        //return the deleted node here
+        temp.next=null;
+        printNode(temp);
+    }
+
+    //delet the  node at the end of the linked list
+    public static void deleteAtTheEndOfLinkedList(Node head){
+    if(head==null || head.next==null)
+    {
+        System.out.println(head.data);
+        return ;
+    }
+    //now we are defining the dat
+    Node pre=null;
+    Node curr=head;
+    while(curr.next!=null){
+        pre=curr;
+        curr=curr.next;
+    }
+    pre.next=null;
+        System.out.println("The remaining nodes are");
+        printNode(head);
+
+    System.out.println("Deleted nodes are "+curr.data);
+    }
+
+    //delete a node at the given positons
+
+
+
 
     //insert a node at the given positions
     public static void insertAtPosition(Node head,int data, int position){
